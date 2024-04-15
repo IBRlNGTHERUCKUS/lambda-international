4f80ae36db5a5277a6df03eb3ff4ebc14c9b1886
Joseph C <josephalexcobian@gmail.com>
2024-04-14 20:51:07 -0700

    Cancel the carousel autoscroll if it has been interacted with.

--- src/index.js
+++ src/index.js
@@ -22,17 +22,28 @@ const domControl = {
         domControl.updateBar();
     }
 }
+//// Auto carousel scroll
+const intervalId = setInterval(domControl.handleCarouselNext, 8_000);
 
 
 const leftBtn = document.querySelector(".left-button");
 const rightBtn = document.querySelector(".right-button");
-leftBtn.addEventListener('click', domControl.handleCarouselPrevious);
-rightBtn.addEventListener('click', domControl.handleCarouselNext)
+leftBtn.addEventListener('click', ()=>{
+        domControl.handleCarouselPrevious();
+        clearInterval(intervalId);
+    }
+);
+rightBtn.addEventListener('click', ()=>{
+        domControl.handleCarouselNext();
+        clearInterval(intervalId);
+    }
+)
 
 const barEls = document.querySelectorAll('.bar');
 for (let i=0; i < barEls.length; i++) {
     barEls[i].addEventListener('click', ()=>{
         domControl.handleCarouselJump(i);
+        clearInterval(intervalId);
     })
 }
 
@@ -45,6 +56,3 @@ function handleExpandable(expandableEl) {
 const heroBgEl = document.querySelector('.hero-bg');
 const projectPara = heroBgEl.querySelector('p');
 heroBgEl.addEventListener('click', ()=>{handleExpandable(projectPara)})
-
-// Auto carousel scroll
-setInterval(domControl.handleCarouselNext, 8_000)
